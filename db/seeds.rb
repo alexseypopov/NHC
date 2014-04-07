02c95736711ff562d9e7c9ae5ad2efb24ed502e3
Alexsey Popov <alexsey.popov718@gmail.com>
2014-04-07 13:10:50 +0300

    db seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'test.nhc', email: 'test.nhc@gmail.com', password: 'test1234')
+User.create(username: 'OSCAR.LUMANOVSKI', email: 'oscar@gmail.com', password: 'test1234')
