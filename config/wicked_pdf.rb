f7c340d908d613b5c03c2fbbdda4a3c31687e191
Alexsey Popov <alexsey.popov718@gmail.com>
2014-04-08 15:51:09 +0300

    ...

--- config/wicked_pdf.rb
+++ config/wicked_pdf.rb
@@ -1,5 +1,6 @@
 if Rails.env.staging? || Rails.env.production?
   exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+  puts exe_path
 else
   exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
   # exe_path = '/usr/local/bin/wkhtmltopdf'
